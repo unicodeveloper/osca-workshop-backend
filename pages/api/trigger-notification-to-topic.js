8c70be887632e113d04bfcc4b50a1e3105281fda
unicodeveloper <prosperotemuyiwa@gmail.com>
2023-06-16 11:00:00 +0100

    fix

---
+++ pages/api/trigger-notification-to-topic.js
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors'
+import { Novu } from '@novu/node';
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD']
+});
+
+const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API_KEY, {
+    backendUrl: process.env.NEXT_BACKEND_API_URL
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req, res) {
+   
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  const { workflowTriggerID, topicKey } = req.body;
+
+  await novu.trigger(workflowTriggerID, {
+    to: [{ type: 'Topic', topicKey: topicKey }],
+    payload: {},
+  });
+    
+  res.json({
+    finish: true 
+  });
+}
